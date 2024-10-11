339f27a277d6d2533d876f98fb86cb2e3a3954f4
Timofey Trofimovich <trofimovichtimofey90210@gmail.com>
2024-10-11 21:45:33 +0300

    update

--- src/main/kotlin/ColorConverter.kt
+++ src/main/kotlin/ColorConverter.kt
@@ -1,70 +1,73 @@
-import kotlin.math.abs
-import kotlin.math.roundToInt
-
 class ColorConverter {
-    fun rgbToCmyk(r: Int, g: Int, b: Int): List<Double> {
-        if (listOf(r, g, b).all { it == 0 }) {
-            return listOf(0.0, 0.0, 0.0, 1.0)
-        }
-        val rNorm = r / 255.0
-        val gNorm = g / 255.0
-        val bNorm = b / 255.0
+    fun rgbToCmyk(r: Int, g: Int, b: Int): List<Int> {
+        val rf = r / 255.0
+        val gf = g / 255.0
+        val bf = b / 255.0
 
-        val k = 1 - maxOf(rNorm, gNorm, bNorm)
-        val c = (1 - rNorm - k) / (1 - k)
-        val m = (1 - gNorm - k) / (1 - k)
-        val y = (1 - bNorm - k) / (1 - k)
+        val k = 1 - maxOf(rf, gf, bf)
+        if (k == 1.0) return listOf(0, 0, 0, 100)
 
-        return listOf(c, m, y, k)
+        val c = ((1 - rf - k) / (1 - k)) * 100
+        val m = ((1 - gf - k) / (1 - k)) * 100
+        val y = ((1 - bf - k) / (1 - k)) * 100
+
+        return listOf(c.toInt(), m.toInt(), y.toInt(), (k * 100).toInt())
     }
 
-    fun cmykToRgb(c: Double, m: Double, y: Double, k: Double): List<Int> {
-        val r = (255 * (1 - c) * (1 - k)).roundToInt()
-        val g = (255 * (1 - m) * (1 - k)).roundToInt()
-        val b = (255 * (1 - y) * (1 - k)).roundToInt()
+    fun cmykToRgb(c: Int, m: Int, y: Int, k: Int): List<Int> {
+        val cRatio = c / 100.0
+        val mRatio = m / 100.0
+        val yRatio = y / 100.0
+        val kRatio = k / 100.0
+
+        val r = (255 * (1 - cRatio) * (1 - kRatio)).toInt()
+        val g = (255 * (1 - mRatio) * (1 - kRatio)).toInt()
+        val b = (255 * (1 - yRatio) * (1 - kRatio)).toInt()
 
         return listOf(r, g, b)
     }
 
-    fun rgbToHsv(r: Int, g: Int, b: Int): List<Double> {
-        val rNorm = r / 255.0
-        val gNorm = g / 255.0
-        val bNorm = b / 255.0
+    fun rgbToHsv(r: Int, g: Int, b: Int): List<Int> {
+        val rf = r / 255.0
+        val gf = g / 255.0
+        val bf = b / 255.0
 
-        val max = maxOf(rNorm, gNorm, bNorm)
-        val min = minOf(rNorm, gNorm, bNorm)
+        val max = maxOf(rf, gf, bf)
+        val min = minOf(rf, gf, bf)
         val delta = max - min
-
-        val v = max
-        val s = if (max == 0.0) 0.0 else delta / max
 
         val h = when {
             delta == 0.0 -> 0.0
-            max == rNorm -> (60 * ((gNorm - bNorm) / delta + 6)) % 360
-            max == gNorm -> 60 * ((bNorm - rNorm) / delta + 2)
-            else -> 60 * ((rNorm - gNorm) / delta + 4)
+            max == rf -> (60 * ((gf - bf) / delta) + 360) % 360
+            max == gf -> (60 * ((bf - rf) / delta) + 120) % 360
+            else -> (60 * ((rf - gf) / delta) + 240) % 360
         }
 
-        return listOf(h, s, v)
+        val s = if (max == 0.0) 0.0 else delta / max * 100
+        val v = max * 100
+
+        return listOf(h.toInt(), s.toInt(), v.toInt())
     }
 
-    fun hsvToRgb(h: Double, s: Double, v: Double): List<Int> {
-        val c = v * s
-        val x = c * (1 - abs((h / 60) % 2 - 1))
-        val m = v - c
+    fun hsvToRgb(h: Int, s: Int, v: Int): List<Int> {
+        val sRatio = s / 100.0
+        val vRatio = v / 100.0
+        val c = vRatio * sRatio
+        val x = c * (1 - Math.abs(h / 60.0 % 2 - 1))
+        val m = vRatio - c
 
-        val (r1, g1, b1) = when {
-            h < 60 -> listOf(c, x, 0.0)
-            h < 120 -> listOf(x, c, 0.0)
-            h < 180 -> listOf(0.0, c, x)
-            h < 240 -> listOf(0.0, x, c)
-            h < 300 -> listOf(x, 0.0, c)
+        val (rf, gf, bf) = when (h) {
+            in 0..59 -> listOf(c, x, 0.0)
+            in 60..119 -> listOf(x, c, 0.0)
+            in 120..179 -> listOf(0.0, c, x)
+            in 180..239 -> listOf(0.0, x, c)
+            in 240..299 -> listOf(x, 0.0, c)
             else -> listOf(c, 0.0, x)
         }
 
-        val r = ((r1 + m) * 255).roundToInt()
-        val g = ((g1 + m) * 255).roundToInt()
-        val b = ((b1 + m) * 255).roundToInt()
+        val r = ((rf + m) * 255).toInt()
+        val g = ((gf + m) * 255).toInt()
+        val b = ((bf + m) * 255).toInt()
 
         return listOf(r, g, b)
     }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,16 @@
-import java.awt.Color
-import java.awt.Dimension
-import java.awt.GridLayout
-import java.util.*
+import java.awt.*
 import javax.swing.*
+import javax.swing.border.TitledBorder
 import javax.swing.event.DocumentEvent
 import javax.swing.event.DocumentListener
 
 class ColorConverterApp : JFrame("Color Converter") {
     private val colorConverter = ColorConverter()
-    private var isUpdating = false;
+    private var isUpdating = false
 
     private var rgbColor = listOf(0, 0, 0)
-    private var cmykColor = listOf(0.0, 0.0, 0.0, 1.0)
-    private var hsvColor = listOf(0.0, 0.0, 0.0)
+    private var cmykColor = listOf(0, 0, 0, 100)
+    private var hsvColor = listOf(0, 0, 0)
 
     private val rgbFields = rgbColor.map { JTextField(5) }
     private val cmykFields = cmykColor.map { JTextField(5) }
@@ -23,41 +21,33 @@ class ColorConverterApp : JFrame("Color Converter") {
     private val hsvSliders = listOf(createSlider(0, 360), createSlider(0, 100), createSlider(0, 100))
 
     private val colorDisplayPanel = JPanel().apply {
-        preferredSize = Dimension(100, 100)
+        preferredSize = Dimension(150, 150)
         background = Color.BLACK
     }
 
     init {
         defaultCloseOperation = EXIT_ON_CLOSE
-        layout = GridLayout(6, 1)
+        layout = GridBagLayout()
+        val gbc = GridBagConstraints().apply { fill = GridBagConstraints.BOTH }
 
-        val rgbPanel = JPanel().apply {
-            add(JLabel("RGB:"))
-            rgbFields.forEach { add(it) }
-            rgbSliders.forEach { add(it) }
-        }
-        add(rgbPanel)
+        addPanelWithBorder("RGB", rgbFields, rgbSliders, gbc, 0)
+        addPanelWithBorder("CMYK", cmykFields, cmykSliders, gbc, 1)
+        addPanelWithBorder("HSV", hsvFields, hsvSliders, gbc, 2)
 
-        val cmykPanel = JPanel().apply {
-            add(JLabel("CMYK:"))
-            cmykFields.forEach { add(it) }
-            cmykSliders.forEach { add(it) }
-        }
-        add(cmykPanel)
+        gbc.gridx = 0
+        gbc.gridy = 3
+        gbc.gridwidth = 2
+        gbc.insets = Insets(10, 10, 10, 10)
+        add(colorDisplayPanel, gbc)
 
-        val hsvPanel = JPanel().apply {
-            add(JLabel("HSV:"))
-            hsvFields.forEach { add(it) }
-            hsvSliders.forEach { add(it) }
-        }
-        add(hsvPanel)
-
-        add(colorDisplayPanel)
-
-        val colorChooserButton = JButton("Choose color").apply {
+        val colorChooserButton = JButton("Choose Color").apply {
             addActionListener { chooseColor() }
         }
-        add(colorChooserButton)
+
+        gbc.gridy = 4
+        gbc.gridwidth = 1
+        gbc.insets = Insets(0, 10, 10, 10)
+        add(colorChooserButton, gbc)
 
         rgbFields.forEach { addDocumentListener(it, ColorScheme.RGB) }
         cmykFields.forEach { addDocumentListener(it, ColorScheme.CMYK) }
@@ -74,22 +64,53 @@ class ColorConverterApp : JFrame("Color Converter") {
         isVisible = true
     }
 
+    private fun addPanelWithBorder(
+        title: String,
+        fields: List<JTextField>,
+        sliders: List<JSlider>,
+        gbc: GridBagConstraints,
+        yPosition: Int
+    ) {
+        val panel = JPanel(GridBagLayout()).apply {
+            border = TitledBorder(title)
+        }
+
+        val gbcInner = GridBagConstraints().apply {
+            gridx = 0
+            gridy = 0
+            insets = Insets(2, 5, 2, 5)
+        }
+
+        fields.forEachIndexed { i, field ->
+            gbcInner.gridx = 0
+            panel.add(field, gbcInner)
+            gbcInner.gridx = 1
+            panel.add(sliders[i], gbcInner)
+            gbcInner.gridy++
+        }
+
+        gbc.gridx = 0
+        gbc.gridy = yPosition
+        gbc.gridwidth = 2
+        add(panel, gbc)
+    }
+
     private fun chooseColor() {
-        val color = JColorChooser.showDialog(this, "Choose color", null)
+        val color = JColorChooser.showDialog(this, "Choose Color", null)
         if (color != null) {
             rgbColor = listOf(color.red, color.green, color.blue)
-            updateColorPanelFromRgb()
+            updateSchemesComponentsFromRgb()
             updateFields()
             updateSliders()
             updateColorDisplay()
         }
     }
 
-    private fun addDocumentListener(field: JTextField, changed: ColorScheme) {
+    private fun addDocumentListener(field: JTextField, scheme: ColorScheme) {
         field.document.addDocumentListener(object : DocumentListener {
-            override fun insertUpdate(e: DocumentEvent?) = updateColors(changed)
-            override fun removeUpdate(e: DocumentEvent?) = updateColors(changed)
-            override fun changedUpdate(e: DocumentEvent?) = updateColors(changed)
+            override fun insertUpdate(e: DocumentEvent?) = updateColors(scheme)
+            override fun removeUpdate(e: DocumentEvent?) = updateColors(scheme)
+            override fun changedUpdate(e: DocumentEvent?) = updateColors(scheme)
         })
     }
 
@@ -102,77 +123,85 @@ class ColorConverterApp : JFrame("Color Converter") {
     }
 
     private fun createSlider(min: Int, max: Int): JSlider =
-        JSlider(min, max)
-            .apply {
-                majorTickSpacing = (max - min) / 5
-                paintTicks = true
-                paintLabels = true
-            }
-
+        JSlider(min, max).apply {
+            majorTickSpacing = (max - min) / 5
+            paintTicks = true
+            paintLabels = true
+        }
 
     private fun updateColors(changed: ColorScheme) {
         if (isUpdating) return
+
         SwingUtilities.invokeLater {
-            isUpdating = true;
+            isUpdating = true
             try {
                 when (changed) {
                     ColorScheme.RGB -> {
-                        if (rgbFields.any { it.text.isEmpty() }) {
-                            return@invokeLater
-                        }
-                        val (r, g, b) = rgbFields.map { it.text.toInt() }
-                        rgbColor = listOf(r, g, b)
-                        updateColorPanelFromRgb()
-                        updateCMYKField()
-                        updateHSVField()
+                        if (inputIsInvalid(rgbFields)) return@invokeLater
+                        updateRGBFromFields()
+                        updateSchemesComponentsFromRgb()
                     }
 
                     ColorScheme.CMYK -> {
-                        if (cmykFields.any { it.text.isEmpty() }) {
-                            return@invokeLater
-                        }
-                        val (c, m, y, k) = cmykFields.map { it.text.toDouble() / 100 }
-                        val (r, g, b) = colorConverter.cmykToRgb(c, m, y, k)
-                        rgbColor = listOf(r, g, b)
-                        cmykColor = listOf(c, m, y, k)
-                        hsvColor = colorConverter.rgbToHsv(r, g, b)
-                        updateRGBField()
-                        updateHSVField()
+                        if (inputIsInvalid(cmykFields)) return@invokeLater
+                        updateRGBFromCmyk()
                     }
 
                     ColorScheme.HSV -> {
-                        if (hsvFields.any { it.text.isEmpty() }) {
-                            return@invokeLater
-                        }
-                        var (h, s, v) = hsvFields.map { it.text.toDouble() }
-                        s /= 100
-                        v /= 100
-                        val (r, g, b) = colorConverter.hsvToRgb(h, s, v)
-                        rgbColor = listOf(r, g, b)
-                        cmykColor = colorConverter.rgbToCmyk(r, g, b)
-                        hsvColor = listOf(h, s, v)
-                        updateRGBField()
-                        updateCMYKField()
+                        if (inputIsInvalid(hsvFields)) return@invokeLater
+                        updateRGBFromHsv()
                     }
                 }
-                updateSliders()
-                updateColorDisplay()
+
+                updateAllFieldsAndDisplay()
             } catch (e: NumberFormatException) {
                 println(e.toString())
             } finally {
-                isUpdating = false;
+                isUpdating = false
             }
         }
     }
-
+    private fun inputIsInvalid(fields: List<JTextField>): Boolean {
+        return if (fields.any { it.text.isNullOrBlank() }) {
+            fields.forEach { it.text = "0" }
+            updateFields()
+            true
+        } else {
+            false
+        }
+    }
+    private fun updateRGBFromFields() {
+        val (r, g, b) = rgbFields.map { it.text.toIntOrNull()?.coerceIn(0, 255) ?: 0 }
+        rgbColor = listOf(r, g, b)
+    }
+    private fun updateRGBFromCmyk() {
+        val (c, m, y, k) = cmykFields.map { it.text.toIntOrNull()?.coerceIn(0, 100) ?: 0 }
+        rgbColor = colorConverter.cmykToRgb(c, m, y, k)
+        cmykColor = listOf(c, m, y, k)
+        hsvColor = colorConverter.rgbToHsv(rgbColor[0], rgbColor[1], rgbColor[2])
+    }
+    private fun updateRGBFromHsv() {
+        val (h, s, v) = hsvFields.mapIndexed { index, field ->
+            val value = field.text.toIntOrNull() ?: 0
+            when (index) {
+                0 -> value.coerceIn(0, 360) // Hue range: 0-360
+                else -> value.coerceIn(0, 100) // Saturation and Value range: 0-100
+            }
+        }
+        rgbColor = colorConverter.hsvToRgb(h, s, v)
+        cmykColor = colorConverter.rgbToCmyk(rgbColor[0], rgbColor[1], rgbColor[2])
+        hsvColor = listOf(h, s, v)
+    }
+    private fun updateAllFieldsAndDisplay() {
+        updateFields()
+        updateSliders()
+        updateColorDisplay()
+    }
     private fun updateRGBField() = updateField(rgbFields, rgbColor)
 
-    private fun updateCMYKField() = updateField(cmykFields, cmykColor.map { it * 100 })
+    private fun updateCMYKField() = updateField(cmykFields, cmykColor)
 
-    private fun updateHSVField() {
-        val (h, s, v) = hsvColor
-        updateField(hsvFields, listOf(h, s * 100, v * 100))
-    }
+    private fun updateHSVField() = updateField(hsvFields, hsvColor)
 
     private fun updateFields() {
         updateRGBField()
@@ -180,47 +209,46 @@ class ColorConverterApp : JFrame("Color Converter") {
         updateHSVField()
     }
 
-    private fun updateField(fields: List<JTextField>, values: List<Number>) {
+    private fun updateField(fields: List<JTextField>, values: List<Int>) {
         fields.forEachIndexed { i, field ->
-            field.text = String.format(if (values[i] is Double) "%.2f" else "%d", values[i])
+            field.text = values[i].toString()
         }
-        println("updated for $values")
     }
 
     private fun updateColorDisplay() {
+        rgbColor.validateComponents()
         val (r, g, b) = rgbColor
-        if (r in 0..255 && g in 0..255 && b in 0..255) {  // Check if r, g, b are within the valid range
-            colorDisplayPanel.background = Color(r, g, b)
-            colorDisplayPanel.repaint()
-        } else {
-            println("RGB values out of range: r=$r, g=$g, b=$b")  // Optional: Log the invalid values
-        }
+        colorDisplayPanel.background = Color(r, g, b)
+        colorDisplayPanel.repaint()
     }
 
+    private fun List<Int>.validateComponents() {
+        rgbColor = this.map { it.coerceIn(0, 255) }
+        updateRGBField()
+        updateCMYKField()
+        updateHSVField()
+    }
 
     private fun updateSliders() {
         val (h, s, v) = hsvColor
         mapOf(
             rgbSliders to rgbColor,
-            cmykSliders to cmykColor.map { it * 100 },
-            hsvSliders to listOf(h, s * 100, v * 100)
+            cmykSliders to cmykColor,
+            hsvSliders to listOf(h, s, v)
         ).forEach { (sliders, values) ->
             sliders.forEachIndexed { i, slider ->
-                slider.value = values[i].toInt()
+                slider.value = values[i]
             }
         }
     }
 
-    private fun updateColorPanelFromRgb() {
+    private fun updateSchemesComponentsFromRgb() {
         val (r, g, b) = rgbColor
-        cmykColor = colorConverter.rgbToCmyk(r, g, b)
-        hsvColor = colorConverter.rgbToHsv(r, g, b)
+        cmykColor = colorConverter.rgbToCmyk(r, g, b).map { it }
+        hsvColor = colorConverter.rgbToHsv(r, g, b).map { it }
     }
 }
 
 fun main() {
-    Locale.setDefault(Locale.ENGLISH)
-    SwingUtilities.invokeLater {
-        ColorConverterApp()
-    }
+    SwingUtilities.invokeLater { ColorConverterApp() }
 }
